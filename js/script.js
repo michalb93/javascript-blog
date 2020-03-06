d86f2187c91c6e5de0ea66d6350b48708022bc15
Michal Borek <borekmichal30@gmail.com>
2020-03-06 21:08:40 +0100

    Add leftbar works

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 'use strict';
 
 function titleClickHandler(event){
-  console.log('Link was clicked!');
-  console.log(event);
-
+event.preventDefault();
+const clickedElement = this;
+ console.log('Link was clicked!');
   /* remove class 'active' from all article links  */
-
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-
-  /* get 'href' attribute from the clicked link */
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-
-  /* add class 'active' to the correct article */
+const activeLinks = document.querySelectorAll('.titles a.active');
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
 }
 
-const links = document.querySelectorAll('.titles a');
+  /* add class 'active' to the clicked link */
+clickedElement.classList.add('active');
+console.log('clickedElement:' ,clickedElement);
 
+  /* remove class 'active' from all articles */
+const activeArticles = document.querySelectorAll('.posts .active');
+for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+}
+  /* get 'href' attribute from the clicked link */
+  const articleSelector = this.getAttribute('href');
+  console.log('Clicked Link:', articleSelector);
+
+  /* find the correct article using the selector (value of 'href' attribute)(nic tu się nie zmienia) */
+const targetArticle = document.querySelector(articleSelector);
+console.log('Article:',targetArticle);
+	
+  /* add class 'active' to the correct article */
+targetArticle.classList.add('active');
+
+};
+
+const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
